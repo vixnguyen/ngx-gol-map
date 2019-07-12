9dceca29c7024b92a31bad6e871fbf769c349249
at-vinguyen <vi.nguyen@asiantech.vn>
2019-07-12 14:35:55 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gol-map-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'golmap';
+}
